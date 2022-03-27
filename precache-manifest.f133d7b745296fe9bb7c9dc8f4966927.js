ce674c0e01a038c93d246e488c6a789f027f9049
安安 <daidaiyidaidaidai@163.com>
2022-03-27 14:50:30 +0800

    deploy

---
+++ precache-manifest.f133d7b745296fe9bb7c9dc8f4966927.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1978c87b4fc3af001f71",
+    "url": "/readily/css/about.26878b5f.css"
+  },
+  {
+    "revision": "7bcaee69e50093dc7c47",
+    "url": "/readily/css/app.708f39d4.css"
+  },
+  {
+    "revision": "538970321325235cb100",
+    "url": "/readily/css/chunk-039c38ba.76c9b84c.css"
+  },
+  {
+    "revision": "27d55d59105fb6cb4545",
+    "url": "/readily/css/chunk-0a3712ce.828ac58c.css"
+  },
+  {
+    "revision": "e2fcd34769189e39ba5d",
+    "url": "/readily/css/chunk-0d4ead71.556ff046.css"
+  },
+  {
+    "revision": "bb6b59b645a5f5e725b8601d73303c72",
+    "url": "/readily/img/noRecord.bb6b59b6.png"
+  },
+  {
+    "revision": "016b53372a2fa3f267a8a046bb472ba8",
+    "url": "/readily/index.html"
+  },
+  {
+    "revision": "1978c87b4fc3af001f71",
+    "url": "/readily/js/about.4f851b33.js"
+  },
+  {
+    "revision": "7bcaee69e50093dc7c47",
+    "url": "/readily/js/app.4209e238.js"
+  },
+  {
+    "revision": "538970321325235cb100",
+    "url": "/readily/js/chunk-039c38ba.0dbbf588.js"
+  },
+  {
+    "revision": "27d55d59105fb6cb4545",
+    "url": "/readily/js/chunk-0a3712ce.3beb1189.js"
+  },
+  {
+    "revision": "e2fcd34769189e39ba5d",
+    "url": "/readily/js/chunk-0d4ead71.ede00d08.js"
+  },
+  {
+    "revision": "2a9bf5a38dfda492e70a",
+    "url": "/readily/js/chunk-vendors.d1961c78.js"
+  },
+  {
+    "revision": "194e4842524ffe6e45ad2c99abdfad4f",
+    "url": "/readily/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/readily/robots.txt"
+  }
+]);
